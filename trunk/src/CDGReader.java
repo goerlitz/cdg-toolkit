87c58b333aafbf3e296fdac55cfbcf5da1c05da8
olaf.goerlitz@gmail.com <olaf.goerlitz@gmail.com@a9ddb9b0-35ba-f76b-1a39-45712136c50a>
2011-11-22 09:46:34 +0000

    initial import

---
+++ trunk/src/CDGReader.java
@@ -0,0 +1,197 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.swing.text.NumberFormatter;
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
+
+public class CDGReader extends TimerTask {
+	
+	public static final String file ="test.cdg";
+	
+	static final int FramesPerSecond = 20;  // frames displayed per second, CD+G has 300 packets per second -> 300/FPS = packets per frame
+	static final int PacketsPerFrame = 300 / FramesPerSecond;
+	static final int FrameInterval   = 1000 / FramesPerSecond;
+	
+	static final byte SC_MASK = 0x3F;
+	static final byte CDG_CMD = 0x09;
+	static final byte CDG_MEMORY_PRESET         =  1;
+	static final byte CDG_BORDER_PRESET         =  2;
+	static final byte CDG_TILE_BLOCK            =  6;
+	static final byte CDG_SCROLL_PRESET         = 20;
+	static final byte CDG_SCROLL_COPY           = 24;
+	static final byte CDG_TRANPARENT_COLOR      = 28;
+	static final byte CDG_LOAD_COLOR_TABLE_LOW  = 30;
+	static final byte CDG_LOAD_COLOR_TABLE_HIGH = 31;
+	static final byte CDG_TILE_BLOCK_XOR        = 38;
+	
+	private CDGViewer viewer = new CDGViewer();
+	private final DataInputStream dis;
+	private Timer timer = new Timer();
+	
+	private byte[] packet = new byte[24];
+	
+	private int[] frequency = new int[39];
+	
+	int packetCount = 0;
+	int cdgCount = 0;
+	
+	int bytesRead;
+	
+	final long start = System.currentTimeMillis();
+	
+
+	
+	public CDGReader() {
+		try {
+			dis = new DataInputStream(new BufferedInputStream(new FileInputStream(file)));
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+			throw new RuntimeException();
+		}
+	}
+	
+	public void process() {
+		timer.scheduleAtFixedRate(this, 0, FrameInterval);
+	}
+
+	public void stop() {
+		timer.cancel();
+		System.out.println("time taken: " + ((System.currentTimeMillis() - start) / 1000) + " seconds");
+		try {
+			dis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		int processedPackets = 0;
+		try {
+//			synchronized(dis) {
+				while ((processedPackets < PacketsPerFrame) && ((bytesRead = dis.read(packet)) == 24)) {
+					processedPackets++;
+					packetCount ++;
+
+					if ((packet[0] & SC_MASK) == CDG_CMD) {   // CD+G?
+						cdgCount++;
+						frequency[packet[1] & SC_MASK]++;
+
+						switch (packet[1] & SC_MASK) {
+						case CDG_MEMORY_PRESET:
+							int color = packet[4] & 0x0F;
+							int repeat = packet[5] & 0x0F;
+							if (repeat == 0) {
+								viewer.clearScreen(color);
+							}
+							break;
+						case CDG_BORDER_PRESET:
+							int bgcol = packet[4] & 0x0F;
+							viewer.clearBorder(bgcol);
+							break;
+						case CDG_LOAD_COLOR_TABLE_LOW:
+							for (int i = 0; i<8; i++) {
+								byte[] cols = decodeColor(packet[4+i*2], packet[5+i*2]);
+								viewer.setColor(cols[0], cols[1], cols[2], i);
+								viewer.applyColor();
+							}
+							break;
+						case CDG_LOAD_COLOR_TABLE_HIGH:
+							for (int i = 0; i<8; i++) {
+								byte[] cols = decodeColor(packet[4+i*2], packet[5+i*2]);
+								viewer.setColor(cols[0], cols[1], cols[2], i+8);
+								viewer.applyColor();
+							}
+							break;
+						case CDG_TILE_BLOCK:
+							paintTile(packet, false);
+							break;
+						case CDG_TILE_BLOCK_XOR:
+							paintTile(packet, true);
+							break;
+						default:
+							break;
+						}
+//						viewer.repaint();
+//						Thread.sleep(100);
+					} else {
+						// not cdg;
+						StringBuffer buf = new StringBuffer();
+						for (byte data : packet) {
+//							int high = data >> 4;
+//							int low = data & 0xF;
+//							buf.append((high < 10) ? high : Character.valueOf((char) (64+high)));
+//							buf.append((low < 10) ? low : Character.valueOf((char) (64+low)));
+							buf.append(data);
+							buf.append(" ");
+						}
+//						System.out.println("NO_CDG: " + buf);
+					}
+				}
+				viewer.repaint();
+				if (bytesRead == -1)
+					stop();
+//			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		//				for (int i = 0; i<=38; i++)
+		//					System.out.println(i + ": " + frequency[i]);
+
+	}
+	
+	private void paintTile(byte[] packet, boolean xor) {
+		int color0 = packet[4] & 0x0F;
+		int color1 = packet[5] & 0x0F;
+		int row    = packet[6] & 0x1F;
+		int column = packet[7] & 0x3F;
+		
+		if (row >= 17 || column >= 49) {
+//			System.out.println("Tile(" + row + "," + column + "): " + color0 + "/" + color1 + (xor ? " XOR" : " ...") + " - OUT OF BOUNDS");
+			return;
+//		} else {
+//			System.out.println("Tile(" + row + "," + column + "): " + color0 + "/" + color1 + (xor ? " XOR" : " ..."));			
+		}
+		
+		row *= 12;
+		column *= 6;
+		
+		for (int y = 0; y < 12; y++) {
+			int scanline = packet[y+8] & 0x3F;
+			for (int x = 5; x >= 0; x--) {
+				int color = ((scanline & 1) == 0) ? color0 : color1;
+				viewer.setPixel(column + x + 3, row + y + 6, color, xor);
+				scanline = scanline >> 1;
+			}
+		}
+	}
+	
+	private void debugCDGInstruction(int count, String msg) {
+		
+	}
+
+	public static void main(String[] args) {
+		new CDGReader().process();
+	}
+	
+	public static byte[] decodeColor(byte high, byte low) {
+		return new byte[] {
+				(byte) ((high & SC_MASK) >> 2),
+				(byte) (((high & 0x3) << 2) | ((low >> 4) & 0x3)),
+				(byte) (low & 0x0F)
+		};
+	}
+
+}
---
+++ trunk/src/CDGViewer.java
@@ -0,0 +1,178 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
+import java.awt.image.IndexColorModel;
+import java.awt.image.Raster;
+import java.awt.image.SampleModel;
+import java.awt.image.SinglePixelPackedSampleModel;
+import java.awt.image.WritableRaster;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+
+public class CDGViewer extends JPanel {
+	
+	private static final int CDG_WIDTH  = 300;
+	private static final int CDG_HEIGHT = 216;
+	
+	private BufferedImage image;// = new BufferedImage(300, 216, BufferedImage.TYPE_BYTE_INDEXED);
+	private DataBuffer dbuf;
+	private IndexColorModel colorModel;
+	
+    byte[] r = new byte[16];
+    byte[] g = new byte[16];
+    byte[] b = new byte[16];
+    
+    private double scale = 2;
+    private int offsetX;
+    private int offsetY;
+	
+	public CDGViewer() {
+		
+        byte[] pixels = new byte[CDG_WIDTH*CDG_HEIGHT];
+
+        // Create a data buffer using the byte buffer of pixel data.
+        // The pixel data is not copied; the data buffer uses the byte buffer array.
+        dbuf = new DataBufferByte(pixels, pixels.length, 0);
+
+        // Prepare a sample model that specifies a storage 4-bits of
+        // pixel datavd in an 8-bit data element
+        int bitMasks[] = new int[]{(byte)0xf};
+        SampleModel sampleModel = new SinglePixelPackedSampleModel(DataBuffer.TYPE_BYTE, CDG_WIDTH, CDG_HEIGHT, bitMasks);
+
+        // Create a raster using the sample model and data buffer
+        WritableRaster raster = Raster.createWritableRaster(sampleModel, dbuf, null);
+
+        // Combine the color model and raster into a buffered image
+        colorModel = generateColorModel();
+        image = new BufferedImage(colorModel, raster, false, null);
+
+		this.setMinimumSize(new Dimension(CDG_WIDTH, CDG_HEIGHT));
+		this.setPreferredSize(new Dimension(CDG_WIDTH*2 , CDG_HEIGHT*2));
+        
+		final JFrame frame = new JFrame("CDG");
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setContentPane(this);
+		frame.pack();
+		
+		this.addComponentListener(new ComponentAdapter() {
+			
+			Dimension dim = new Dimension();
+			
+			@Override
+			public void componentResized(ComponentEvent e) {
+				frame.getContentPane().getSize(dim);
+				double scaleX = (double) dim.getWidth() / CDG_WIDTH;
+				double scaleY = (double) dim.getHeight() / CDG_HEIGHT;
+				if (Double.compare(scaleX, scaleY) < 0) {
+					scale = scaleX;
+					offsetX = 0;
+					offsetY = (int) (dim.getHeight() - CDG_HEIGHT*scale) / 2;
+				} else {
+					scale = scaleY;
+					offsetX = (int) (dim.getWidth() - CDG_WIDTH*scale) / 2;
+					offsetY = 0;					
+				}
+				frame.repaint();
+			}
+		});
+
+		frame.setVisible(true);
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+
+//		// test
+//		Graphics2D g2d=(Graphics2D) image.getGraphics();
+//		Font myFont=new Font("Arial", Font.BOLD|Font.PLAIN, 15);
+//		String s="I GOTTA FEELING";
+//		g2d.setFont( myFont ); //Schriftart setzen
+//		g2d.drawString(s,70,79); //String rendern
+		
+		// scale image to screen size
+		AffineTransform tx = new AffineTransform();
+		tx.scale(scale, scale);
+		AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+		BufferedImage scaled = op.filter(image, null);
+		g.drawImage(scaled, offsetX, offsetY, null);
+	}
+	
+	public void setColor(byte r, byte g, byte b, int index) {
+//		this.r[index] = (byte) (r * 17);
+//		this.g[index] = (byte) (g * 17);
+//		this.b[index] = (byte) (b * 17);
+		this.r[index] = (byte) (r << 4 | r);
+		this.g[index] = (byte) (g << 4 | g);
+		this.b[index] = (byte) (b << 4 | b);
+//		System.out.println("Assign color: " + index + ": " + this.r[index] + "/" + this.g[index] + "/" + this.b[index]);
+	}
+	
+	public void applyColor() {
+		this.image = new BufferedImage(new IndexColorModel(4, 16, this.r, this.g, this.b), image.getRaster(), false, null);
+		repaint();
+	}
+	
+	public void setPixel(int x, int y, int color, boolean xor) {
+		if (xor) {
+			color ^= image.getRaster().getDataBuffer().getElem(y*300+x);
+		}
+		image.getRaster().getDataBuffer().setElem(y*300+x, color);
+	}
+	
+//	public void setPixelXOR(int x, int y, int color) {
+//		int val = image.getRaster().getDataBuffer().getElem(y*300+x);
+//		image.getRaster().getDataBuffer().setElem(y*300+x, color ^ val);
+//	}
+	
+	public void clearBorder(int col) {
+//		System.out.println("Clear Border with color: " + col + ": " + (0xFF & r[col]) + "/" + (0xFF & g[col]) + "/" + (0xFF & b[col]));
+		this.setBackground(new Color(0xFF & r[col], 0xFF & g[col], 0xFF & b[col]));
+		int pos = 0;
+		for (int y = 0; y < 6; y++) {
+			for (int x = 0; x < 300; x++) {
+				image.getRaster().getDataBuffer().setElem(pos++, col);
+			}
+		}
+		for (int y = 6; y < 210; y++) {
+			for (int x = 0; x < 3; x++) {
+				image.getRaster().getDataBuffer().setElem(pos++, col);
+			}
+			pos += 294;
+			for (int x = 297; x < 300; x++) {
+				image.getRaster().getDataBuffer().setElem(pos++, col);
+			}			
+		}
+		for (int y = 0; y < 6; y++) {
+			for (int x = 0; x < 300; x++) {
+				image.getRaster().getDataBuffer().setElem(pos++, col);
+			}
+		}
+	}
+	
+	public void clearScreen(int col) {
+//		System.out.println("Clear Screen with color: " + col + ": " + (0xFF & r[col]) + "/" + (0xFF & g[col]) + "/" + (0xFF & b[col]));
+		for (int y = 6; y < 210; y++) {
+			for (int x = 3; x < 297; x++) {
+				image.getRaster().getDataBuffer().setElem(y*300+x, col);
+			}
+		}
+	}
+	
+    private IndexColorModel generateColorModel() {
+        return new IndexColorModel(4, 16, r, g, b);
+    }
+
+}
